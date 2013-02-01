994de6924f367530400d4a246105deaa8bb5e893
Thomas Borg Salling <tbsalling@tbsalling.dk>
2013-02-01 20:31:48 +0100

    Removing @-signs and stripping strings.

--- src/main/java/dk/tbsalling/aismessages/decoder/DecoderImpl.java
+++ src/main/java/dk/tbsalling/aismessages/decoder/DecoderImpl.java
@@ -62,7 +62,7 @@ public class DecoderImpl implements Decoder {
 	static {
 		StringBuffer sb = new StringBuffer();
 		sb.append("\n");
-		sb.append("AISMessages v1.02 // Copyright (c) 2011-13 by S-Consult ApS, Denmark, CVR DK31327490. http://s-consult.dk.\n");
+		sb.append("AISMessages v1.03 // Copyright (c) 2011-13 by S-Consult ApS, Denmark, CVR DK31327490. http://s-consult.dk.\n");
 		sb.append("\n");
 		sb.append("This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of\n");
 		sb.append("this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 171 Second Street,\n");
