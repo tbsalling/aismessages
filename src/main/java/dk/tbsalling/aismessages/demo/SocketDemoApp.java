03cab549f7a951e52890f7e64b3e2a927e6be420
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-11-28 12:26:48 +0100

    Updated free AIS demo source to ais.exploratorium:80

--- src/main/java/dk/tbsalling/aismessages/demo/SocketDemoApp.java
+++ src/main/java/dk/tbsalling/aismessages/demo/SocketDemoApp.java
@@ -36,7 +36,8 @@ public class SocketDemoApp implements Consumer<AISMessage> {
 		System.out.println("--------------------");
 
 		try {
-			NMEAMessageSocketClient nmeaMessageHandler = new NMEAMessageSocketClient("207.7.148.216", 9009, new NMEAMessageHandler("DEMOSRC1", this));
+			// NMEAMessageSocketClient nmeaMessageHandler = new NMEAMessageSocketClient("207.7.148.216", 9009, new NMEAMessageHandler("DEMOSRC1", this));
+			NMEAMessageSocketClient nmeaMessageHandler = new NMEAMessageSocketClient("ais.exploratorium.edu", 80, new NMEAMessageHandler("DEMOSRC1", this));
 			nmeaMessageHandler.run();
 		} catch (UnknownHostException e) {
 			System.err.println("Unknown host: " + e.getMessage());
