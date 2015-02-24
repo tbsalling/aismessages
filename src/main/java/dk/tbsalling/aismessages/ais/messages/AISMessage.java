aeaaed740783a77aedb1198c53cbb4064df7de28
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-02-24 17:02:27 +0100

    Version no. corrected.

--- src/main/java/dk/tbsalling/aismessages/ais/messages/AISMessage.java
+++ src/main/java/dk/tbsalling/aismessages/ais/messages/AISMessage.java
@@ -48,7 +48,7 @@ public abstract class AISMessage implements Serializable, CachedDecodedValues {
 
     private transient static final Logger LOG = Logger.getLogger(AISMessage.class.getName());
 
-    public transient static final String VERSION = "2.0.0-SNAPSHOT";
+    public transient static final String VERSION = "2.1.1";
 
     static {
         System.err.print("\n" + "AISMessages v" + VERSION + " // Copyright (c) 2011- by S-Consult ApS, Denmark, CVR DK31327490. http://s-consult.dk.\n" + "\n" + "This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of\n" + "this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 171 Second Street,\n" + "Suite 300, San Francisco, California, 94105, USA.\n" + "\n" + "NOT FOR COMMERCIAL USE!\n" + "Contact sales@s-consult.dk to obtain commercially licensed software.\n" + "\n");
