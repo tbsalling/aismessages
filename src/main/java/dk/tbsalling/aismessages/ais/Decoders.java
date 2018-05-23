505b79507dbb8920e3515e0082ff6b193c6b7b0b
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-05-23 20:58:48 +0200

    Use lambda expressions where possible

--- src/main/java/dk/tbsalling/aismessages/ais/Decoders.java
+++ src/main/java/dk/tbsalling/aismessages/ais/Decoders.java
@@ -22,6 +22,77 @@ import java.util.function.Function;
 
 public class Decoders {
 
+    private final static Map<Integer, String> SIX_BIT_ASCII = new TreeMap<Integer, String>();
+
+    static {
+        SIX_BIT_ASCII.put(0, "@"); // 0
+        SIX_BIT_ASCII.put(1, "A"); // 1
+        SIX_BIT_ASCII.put(2, "B"); // 2
+        SIX_BIT_ASCII.put(3, "C"); // 3
+        SIX_BIT_ASCII.put(4, "D"); // 4
+        SIX_BIT_ASCII.put(5, "E"); // 5
+        SIX_BIT_ASCII.put(6, "F"); // 6
+        SIX_BIT_ASCII.put(7, "G"); // 7
+        SIX_BIT_ASCII.put(8, "H"); // 8
+        SIX_BIT_ASCII.put(9, "I"); // 9
+        SIX_BIT_ASCII.put(10, "J"); // 10
+        SIX_BIT_ASCII.put(11, "K"); // 11
+        SIX_BIT_ASCII.put(12, "L"); // 12
+        SIX_BIT_ASCII.put(13, "M"); // 13
+        SIX_BIT_ASCII.put(14, "N"); // 14
+        SIX_BIT_ASCII.put(15, "O"); // 15
+        SIX_BIT_ASCII.put(16, "P"); // 16
+        SIX_BIT_ASCII.put(17, "Q"); // 17
+        SIX_BIT_ASCII.put(18, "R"); // 18
+        SIX_BIT_ASCII.put(19, "S"); // 19
+        SIX_BIT_ASCII.put(20, "T"); // 20
+        SIX_BIT_ASCII.put(21, "U"); // 21
+        SIX_BIT_ASCII.put(22, "V"); // 22
+        SIX_BIT_ASCII.put(23, "W"); // 23
+        SIX_BIT_ASCII.put(24, "X"); // 24
+        SIX_BIT_ASCII.put(25, "Y"); // 25
+        SIX_BIT_ASCII.put(26, "Z"); // 26
+        SIX_BIT_ASCII.put(27, "["); // 27
+        SIX_BIT_ASCII.put(28, "\\"); // 28
+        SIX_BIT_ASCII.put(29, "]"); // 29
+        SIX_BIT_ASCII.put(30, "^"); // 30
+        SIX_BIT_ASCII.put(31, "_"); // 31
+        SIX_BIT_ASCII.put(32, " "); // 32
+        SIX_BIT_ASCII.put(33, "!"); // 33
+        SIX_BIT_ASCII.put(34, "\""); // 34
+        SIX_BIT_ASCII.put(35, "#"); // 35
+        SIX_BIT_ASCII.put(36, "$"); // 36
+        SIX_BIT_ASCII.put(37, "%"); // 37
+        SIX_BIT_ASCII.put(38, "&"); // 38
+        SIX_BIT_ASCII.put(39, "'"); // 39
+        SIX_BIT_ASCII.put(40, "("); // 40
+        SIX_BIT_ASCII.put(41, ")"); // 41
+        SIX_BIT_ASCII.put(42, "*"); // 42
+        SIX_BIT_ASCII.put(43, "+"); // 43
+        SIX_BIT_ASCII.put(44, ","); // 44
+        SIX_BIT_ASCII.put(45, "-"); // 45
+        SIX_BIT_ASCII.put(46, "."); // 46
+        SIX_BIT_ASCII.put(47, "/"); // 47
+        SIX_BIT_ASCII.put(48, "0"); // 48
+        SIX_BIT_ASCII.put(49, "1"); // 49
+        SIX_BIT_ASCII.put(50, "2"); // 50
+        SIX_BIT_ASCII.put(51, "3"); // 51
+        SIX_BIT_ASCII.put(52, "4"); // 52
+        SIX_BIT_ASCII.put(53, "5"); // 53
+        SIX_BIT_ASCII.put(54, "6"); // 54
+        SIX_BIT_ASCII.put(55, "7"); // 55
+        SIX_BIT_ASCII.put(56, "8"); // 56
+        SIX_BIT_ASCII.put(57, "9"); // 57
+        SIX_BIT_ASCII.put(58, ":"); // 58
+        SIX_BIT_ASCII.put(59, ";"); // 59
+        SIX_BIT_ASCII.put(60, "<"); // 60
+        SIX_BIT_ASCII.put(61, "="); // 61
+        SIX_BIT_ASCII.put(62, ">"); // 62
+        SIX_BIT_ASCII.put(63, "?"); // 63
+    }
+
+    private static final Boolean STRIP_ALPHA_SIGNS = true;
+
     public static final Function<String, Integer> INTEGER_DECODER = new Function<String, Integer>() {
         @Override
         public Integer apply(String bitString) {
@@ -50,26 +121,21 @@ public class Decoders {
 
     public static final Function<String, Float> UNSIGNED_FLOAT_DECODER = bitString -> Float.valueOf(UNSIGNED_INTEGER_DECODER.apply(bitString));
 
-    public static final Function<String, String> TIME_DECODER = new Function<String, String>() {
-        @Override
-        public String apply(String bitString) {
-            Integer month = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(0, 4));
-            Integer day = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(4, 9));
-            Integer hour = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(9, 14));
-            Integer minute = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(14, 20));
+    public static final Function<String, String> TIME_DECODER = bitString -> {
+        Integer month = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(0, 4));
+        Integer day = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(4, 9));
+        Integer hour = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(9, 14));
+        Integer minute = UNSIGNED_INTEGER_DECODER.apply(bitString.substring(14, 20));
 
-            String monthAsString = month<10 ? "0"+month : ""+month;
-            String dayAsString = day<10 ? "0"+day : ""+day;
-            String hourAsString = hour<10 ? "0"+hour : ""+hour;
-            String minuteAsString = minute<10 ? "0"+minute : ""+minute;
+        String monthAsString = month<10 ? "0"+month : ""+month;
+        String dayAsString = day<10 ? "0"+day : ""+day;
+        String hourAsString = hour<10 ? "0"+hour : ""+hour;
+        String minuteAsString = minute<10 ? "0"+minute : ""+minute;
 
-            return dayAsString + "-" + monthAsString + " " + hourAsString + ":" + minuteAsString;
-        }
+        return dayAsString + "-" + monthAsString + " " + hourAsString + ":" + minuteAsString;
     };
 
-    public static final Function<String, String> STRING_DECODER = new Function<String, String>() {
-        @Override
-        public String apply(String bitString) {
+    public static final Function<String, String> STRING_DECODER = bitString -> {
             StringBuffer stringBuffer = new StringBuffer();
             String remainingBits = bitString;
             while (remainingBits.length() >= 6) {
@@ -84,79 +150,8 @@ public class Decoders {
                 string = string.replaceAll("@", " ").trim();
             }
             return string;
-        }
     };
 
     public static final Function<String, String> BIT_DECODER = bitString -> bitString;
-
-	private final static Map<Integer, String> SIX_BIT_ASCII = new TreeMap<Integer, String>();
-	static {
-		SIX_BIT_ASCII.put(0, "@"); // 0
-		SIX_BIT_ASCII.put(1, "A"); // 1
-		SIX_BIT_ASCII.put(2, "B"); // 2
-		SIX_BIT_ASCII.put(3, "C"); // 3
-		SIX_BIT_ASCII.put(4, "D"); // 4
-		SIX_BIT_ASCII.put(5, "E"); // 5
-		SIX_BIT_ASCII.put(6, "F"); // 6
-		SIX_BIT_ASCII.put(7, "G"); // 7
-		SIX_BIT_ASCII.put(8, "H"); // 8
-		SIX_BIT_ASCII.put(9, "I"); // 9
-		SIX_BIT_ASCII.put(10, "J"); // 10
-		SIX_BIT_ASCII.put(11, "K"); // 11
-		SIX_BIT_ASCII.put(12, "L"); // 12
-		SIX_BIT_ASCII.put(13, "M"); // 13
-		SIX_BIT_ASCII.put(14, "N"); // 14
-		SIX_BIT_ASCII.put(15, "O"); // 15
-		SIX_BIT_ASCII.put(16, "P"); // 16
-		SIX_BIT_ASCII.put(17, "Q"); // 17
-		SIX_BIT_ASCII.put(18, "R"); // 18
-		SIX_BIT_ASCII.put(19, "S"); // 19
-		SIX_BIT_ASCII.put(20, "T"); // 20
-		SIX_BIT_ASCII.put(21, "U"); // 21
-		SIX_BIT_ASCII.put(22, "V"); // 22
-		SIX_BIT_ASCII.put(23, "W"); // 23
-		SIX_BIT_ASCII.put(24, "X"); // 24
-		SIX_BIT_ASCII.put(25, "Y"); // 25
-		SIX_BIT_ASCII.put(26, "Z"); // 26
-		SIX_BIT_ASCII.put(27, "["); // 27
-		SIX_BIT_ASCII.put(28, "\\"); // 28
-		SIX_BIT_ASCII.put(29, "]"); // 29
-		SIX_BIT_ASCII.put(30, "^"); // 30
-		SIX_BIT_ASCII.put(31, "_"); // 31
-		SIX_BIT_ASCII.put(32, " "); // 32
-		SIX_BIT_ASCII.put(33, "!"); // 33
-		SIX_BIT_ASCII.put(34, "\""); // 34
-		SIX_BIT_ASCII.put(35, "#"); // 35
-		SIX_BIT_ASCII.put(36, "$"); // 36
-		SIX_BIT_ASCII.put(37, "%"); // 37
-		SIX_BIT_ASCII.put(38, "&"); // 38
-		SIX_BIT_ASCII.put(39, "'"); // 39
-		SIX_BIT_ASCII.put(40, "("); // 40
-		SIX_BIT_ASCII.put(41, ")"); // 41
-		SIX_BIT_ASCII.put(42, "*"); // 42
-		SIX_BIT_ASCII.put(43, "+"); // 43
-		SIX_BIT_ASCII.put(44, ","); // 44
-		SIX_BIT_ASCII.put(45, "-"); // 45
-		SIX_BIT_ASCII.put(46, "."); // 46
-		SIX_BIT_ASCII.put(47, "/"); // 47
-		SIX_BIT_ASCII.put(48, "0"); // 48
-		SIX_BIT_ASCII.put(49, "1"); // 49
-		SIX_BIT_ASCII.put(50, "2"); // 50
-		SIX_BIT_ASCII.put(51, "3"); // 51
-		SIX_BIT_ASCII.put(52, "4"); // 52
-		SIX_BIT_ASCII.put(53, "5"); // 53
-		SIX_BIT_ASCII.put(54, "6"); // 54
-		SIX_BIT_ASCII.put(55, "7"); // 55
-		SIX_BIT_ASCII.put(56, "8"); // 56
-		SIX_BIT_ASCII.put(57, "9"); // 57
-		SIX_BIT_ASCII.put(58, ":"); // 58
-		SIX_BIT_ASCII.put(59, ";"); // 59
-		SIX_BIT_ASCII.put(60, "<"); // 60
-		SIX_BIT_ASCII.put(61, "="); // 61
-		SIX_BIT_ASCII.put(62, ">"); // 62
-		SIX_BIT_ASCII.put(63, "?"); // 63
-	}
-
-    private static final Boolean STRIP_ALPHA_SIGNS = true;
 
 }
