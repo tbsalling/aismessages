c59aff8c548a2652ef6b21ec643b8a475a7ee02e
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-09-22 15:51:08 +0200

    Reestablish UnknownApplicationSpecificMessage test

--- src/test/java/dk/tbsalling/aismessages/ais/messages/BinaryBroadcastMessageTest.java
+++ src/test/java/dk/tbsalling/aismessages/ais/messages/BinaryBroadcastMessageTest.java
@@ -29,12 +29,12 @@ public class BinaryBroadcastMessageTest {
 
     @Test
     public void canDecodeUnknownApplicationSpecificMessage() {
-        AISMessage aisMessage = AISMessage.create(NMEAMessage.fromString("!AIVDM,1,1,,A,83aDCr@j2P000000029Pt?cm0000,0*5F"));
+        AISMessage aisMessage = AISMessage.create(NMEAMessage.fromString("!AIVDM,1,1,,A,8@30oni?1j020@00,0*23"));
 
         System.out.println(aisMessage.toString());
 
-        assertEquals(200, ((BinaryBroadcastMessage) aisMessage).getDesignatedAreaCode().intValue());
-        assertEquals(10, ((BinaryBroadcastMessage) aisMessage).getFunctionalId().intValue());
+        assertEquals(316, ((BinaryBroadcastMessage) aisMessage).getDesignatedAreaCode().intValue());
+        assertEquals(7, ((BinaryBroadcastMessage) aisMessage).getFunctionalId().intValue());
         assertEquals(UnknownApplicationSpecificMessage.class, ((BinaryBroadcastMessage) aisMessage).getApplicationSpecificMessage().getClass());
     }
 
