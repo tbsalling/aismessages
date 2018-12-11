19458747c5980909c3fb43a641a7d320f0204fb9
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-12-11 11:14:24 +0100

    export required packages for AISmessages to work as a Java module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
-module dk.tbsalling.ais.aismessages {
+module dk.tbsalling.ais.messages {
     exports dk.tbsalling.aismessages;
+    exports dk.tbsalling.aismessages.ais;
+    exports dk.tbsalling.aismessages.ais.exceptions;
+    exports dk.tbsalling.aismessages.ais.messages;
+    exports dk.tbsalling.aismessages.ais.messages.types;
+    exports dk.tbsalling.aismessages.ais.messages.asm;
 }
