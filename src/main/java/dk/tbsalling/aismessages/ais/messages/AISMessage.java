168dd016865b54bc6847c9b36c6b100437a50f34
Thomas Borg Salling <tbsalling@tbsalling.dk>
2020-10-15 16:18:29 +0200

    Updated copyright notice and contact information

--- src/main/java/dk/tbsalling/aismessages/ais/messages/AISMessage.java
+++ src/main/java/dk/tbsalling/aismessages/ais/messages/AISMessage.java
@@ -3,14 +3,14 @@
  * - a java-based library for decoding of AIS messages from digital VHF radio traffic related
  * to maritime navigation and safety in compliance with ITU 1371.
  *
- * (C) Copyright 2011-2013 by S-Consult ApS, DK31327490, http://s-consult.dk, Denmark.
+ * (C) Copyright 2011- by S-Consult ApS, VAT no. DK31327490, Denmark.
  *
  * Released under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
  * For details of this license see the nearby LICENCE-full file, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
  * or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
  *
  * NOT FOR COMMERCIAL USE!
- * Contact sales@s-consult.dk to obtain a commercially licensed version of this software.
+ * Contact Thomas Borg Salling <tbsalling@tbsalling.dk> to obtain a commercially licensed version of this software.
  *
  */
 
@@ -58,7 +58,7 @@ public abstract class AISMessage implements Serializable, CachedDecodedValues {
     public transient static final String VERSION = "3.0.0";
 
     static {
-        System.err.print("\n" + "AISMessages v" + VERSION + " // Copyright (c) 2011- by S-Consult ApS, Denmark, CVR DK31327490. http://s-consult.dk.\n" + "\n" + "This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of\n" + "this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 171 Second Street,\n" + "Suite 300, San Francisco, California, 94105, USA.\n" + "\n" + "NOT FOR COMMERCIAL USE!\n" + "Contact sales@s-consult.dk to obtain commercially licensed software.\n" + "\n");
+        System.err.print("\n" + "AISMessages v" + VERSION + " // Copyright (c) 2011- by S-Consult ApS, Denmark, CVR DK31327490. http://tbsalling.dk.\n" + "\n" + "This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of\n" + "this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 171 Second Street,\n" + "Suite 300, San Francisco, California, 94105, USA.\n" + "\n" + "NOT FOR COMMERCIAL USE!\n" + "Contact Thomas Borg Salling <tbsalling@tbsalling.dk> to obtain commercially licensed software.\n" + "\n");
     }
 
     /**
--- src/main/java/dk/tbsalling/aismessages/ais/messages/StaticDataReport.java
+++ src/main/java/dk/tbsalling/aismessages/ais/messages/StaticDataReport.java
@@ -2,16 +2,16 @@
  * AISMessages
  * - a java-based library for decoding of AIS messages from digital VHF radio traffic related
  * to maritime navigation and safety in compliance with ITU 1371.
- * 
- * (C) Copyright 2011-2013 by S-Consult ApS, DK31327490, http://s-consult.dk, Denmark.
- * 
+ *
+ * (C) Copyright 2011- by S-Consult ApS, VAT no. DK31327490, Denmark.
+ *
  * Released under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
  * For details of this license see the nearby LICENCE-full file, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
  * or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
- * 
+ *
  * NOT FOR COMMERCIAL USE!
- * Contact sales@s-consult.dk to obtain a commercially licensed version of this software.
- * 
+ * Contact Thomas Borg Salling <tbsalling@tbsalling.dk> to obtain a commercially licensed version of this software.
+ * *
  */
 
 package dk.tbsalling.aismessages.ais.messages;
