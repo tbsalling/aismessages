114c17e294d32169d64199bc9f122afe87d17178
Thomas Borg Salling <tbsalling@tbsalling.dk>
2021-02-22 23:02:06 +0100

    Added unit test for legacy create(...)
    Signed-off-by: Thomas Borg Salling <tbsalling@tbsalling.dk>

--- src/test/java/dk/tbsalling/aismessages/ais/messages/AISMessageTest.java
+++ src/test/java/dk/tbsalling/aismessages/ais/messages/AISMessageTest.java
@@ -12,10 +12,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.Instant;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AISMessageTest {
 
@@ -26,7 +23,7 @@ public class AISMessageTest {
     }
 
     @Test
-    public void testEqualsWithMetadata() {
+    public void testEqualsWithMetadataAndTagBlock() {
         String source = "test";
         Instant time = Instant.now();
         NMEAMessage nmea = NMEAMessage.fromString("!AIVDM,1,1,,A,13aEOK?P00PD2wVMdLDRhgvL289?,0*26");
@@ -39,6 +36,27 @@ public class AISMessageTest {
         AISMessage ais1 = AISMessage.create(meta1, tag, nmea);
         AISMessage ais2 = AISMessage.create(meta2, tag, nmea);
         AISMessage ais3 = AISMessage.create(meta3, tag, nmea);
+
+        assertEquals(meta1, meta2);
+        assertNotEquals(meta1, meta3);
+
+        assertEquals(ais1, ais2);
+        assertNotEquals(ais1, ais3);
+    }
+
+    @Test
+    public void testEqualsWithMetadata() {
+        String source = "test";
+        Instant time = Instant.now();
+        NMEAMessage nmea = NMEAMessage.fromString("!AIVDM,1,1,,A,13aEOK?P00PD2wVMdLDRhgvL289?,0*26");
+
+        Metadata meta1 = new Metadata(source, time);
+        Metadata meta2 = new Metadata(source, time);
+        Metadata meta3 = new Metadata(source, time.plusMillis(1000));
+
+        AISMessage ais1 = AISMessage.create(meta1, nmea);
+        AISMessage ais2 = AISMessage.create(meta2, nmea);
+        AISMessage ais3 = AISMessage.create(meta3, nmea);
 
         assertEquals(meta1, meta2);
         assertNotEquals(meta1, meta3);
