cc6a84df55fdf1b5cfb0bca78ebcd5c4570dc130
Thomas Borg Salling <tbsalling@tbsalling.dk>
2021-06-23 07:32:30 +0200

    Added unit test for Github issue #38

--- src/test/java/dk/tbsalling/aismessages/ais/messages/ClassBCSStaticDataReportTest.java
+++ src/test/java/dk/tbsalling/aismessages/ais/messages/ClassBCSStaticDataReportTest.java
@@ -4,6 +4,7 @@ import dk.tbsalling.aismessages.ais.messages.types.AISMessageType;
 import dk.tbsalling.aismessages.ais.messages.types.MMSI;
 import dk.tbsalling.aismessages.ais.messages.types.ShipType;
 import dk.tbsalling.aismessages.nmea.messages.NMEAMessage;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -33,4 +34,15 @@ public class ClassBCSStaticDataReportTest {
         assertEquals(MMSI.valueOf(8417347), message.getMothershipMmsi());
 
     }
+
+    @Test
+    @Ignore
+    public void canDecode_githubIssue38() {
+        String msg = "!AIVDM,1,1,,B,H3m9T21HTe<H`u8B22222222220,0*0F";
+        NMEAMessage nmeaMessage = NMEAMessage.fromString(msg);
+        AISMessage aisMessage = AISMessage.create(nmeaMessage);
+
+        System.out.println(aisMessage.toString());
+    }
+
 }
