02e07e7cb146fe535f26061ea9ff2cc9c8926381
Thomas Borg Salling <tbsalling@tbsalling.dk>
2022-09-29 21:03:22 +0200

    Updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module dk.tbsalling.ais.messages {
-    exports dk.tbsalling.aismessages;
-    exports dk.tbsalling.aismessages.ais;
     exports dk.tbsalling.aismessages.ais.exceptions;
-    exports dk.tbsalling.aismessages.ais.messages;
-    exports dk.tbsalling.aismessages.ais.messages.types;
     exports dk.tbsalling.aismessages.ais.messages.asm;
+    exports dk.tbsalling.aismessages.ais.messages.types;
+    exports dk.tbsalling.aismessages.ais.messages;
+    exports dk.tbsalling.aismessages.ais;
+    exports dk.tbsalling.aismessages.nmea.exceptions;
+    exports dk.tbsalling.aismessages.nmea.messages;
+    exports dk.tbsalling.aismessages.nmea;
+    exports dk.tbsalling.aismessages;
 }
