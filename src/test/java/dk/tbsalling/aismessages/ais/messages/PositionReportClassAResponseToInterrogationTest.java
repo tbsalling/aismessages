3a3cf854598a2b69a0f736b21b719b0a81fde3d9
Thomas Borg Salling <tbsalling@tbsalling.dk>
2023-04-21 07:53:08 +0200

    Added unit test to PositionReportClassAResponseToInterrogationTest

--- src/test/java/dk/tbsalling/aismessages/ais/messages/PositionReportClassAResponseToInterrogationTest.java
+++ src/test/java/dk/tbsalling/aismessages/ais/messages/PositionReportClassAResponseToInterrogationTest.java
@@ -20,22 +20,49 @@ public class PositionReportClassAResponseToInterrogationTest {
         System.out.println(aisMessage.toString());
 
         assertEquals(AISMessageType.PositionReportClassAResponseToInterrogation, aisMessage.getMessageType());
-        assertEquals((Integer) 0, aisMessage.getRepeatIndicator());
+        assertEquals(0, aisMessage.getRepeatIndicator());
         PositionReportClassAResponseToInterrogation message = (PositionReportClassAResponseToInterrogation) aisMessage;
         assertEquals(MMSI.valueOf(304911000), message.getSourceMmsi());
         assertEquals(NavigationStatus.UnderwayUsingEngine, message.getNavigationStatus());
-        assertEquals((Integer) 0, message.getRateOfTurn());
-        assertEquals((Float) 13.6f, message.getSpeedOverGround());
-        assertEquals((Integer) 136, message.getRawSpeedOverGround());
+        assertEquals(0, message.getRateOfTurn());
+        assertEquals(13.6f, message.getSpeedOverGround());
+        assertEquals(136, message.getRawSpeedOverGround());
         assertTrue(message.getPositionAccuracy());
-        assertEquals(Float.valueOf(37.21113f), message.getLatitude());
-        assertEquals((Integer)22326676, message.getRawLatitude());
-        assertEquals(Float.valueOf(-123.45053f), message.getLongitude());
-        assertEquals((Integer)(-74070321), message.getRawLongitude());
-        assertEquals(Float.valueOf(329.7f), message.getCourseOverGround());
-        assertEquals((Integer) 3297, message.getRawCourseOverGround());
-        assertEquals((Integer) 331, message.getTrueHeading());
-        assertEquals((Integer) 7, message.getSecond());
+        assertEquals(37.21113f, message.getLatitude());
+        assertEquals(22326676, message.getRawLatitude());
+        assertEquals(-123.45053f, message.getLongitude());
+        assertEquals(-74070321, message.getRawLongitude());
+        assertEquals(329.7f, message.getCourseOverGround());
+        assertEquals(3297, message.getRawCourseOverGround());
+        assertEquals(331, message.getTrueHeading());
+        assertEquals(7, message.getSecond());
+        assertEquals(ManeuverIndicator.NotAvailable, message.getSpecialManeuverIndicator());
+        assertFalse(message.getRaimFlag());
+    }
+
+    @Test
+    public void canDecode2() {
+        AISMessage aisMessage = AISMessage.create(NMEAMessage.fromString("!AIVDM,1,1,,A,3:U70chP@7LrG1SjrgmKF8uh00vP,0*56"));
+
+        System.out.println(aisMessage.toString());
+
+        assertEquals(AISMessageType.PositionReportClassAResponseToInterrogation, aisMessage.getMessageType());
+        assertEquals(0, aisMessage.getRepeatIndicator());
+        PositionReportClassAResponseToInterrogation message = (PositionReportClassAResponseToInterrogation) aisMessage;
+        assertEquals(MMSI.valueOf(710000815), message.getSourceMmsi());
+        assertEquals(NavigationStatus.UnderwayUsingEngine, message.getNavigationStatus());
+        assertEquals(-720, message.getRateOfTurn());
+        assertEquals(0.7f, message.getSpeedOverGround());
+        assertEquals(7, message.getRawSpeedOverGround());
+        assertFalse(message.getPositionAccuracy());
+        assertEquals(-22.862577f, message.getLatitude());
+        assertEquals(-13717547, message.getRawLatitude());
+        assertEquals(-43.175175f, message.getLongitude());
+        assertEquals(-25905103, message.getRawLongitude());
+        assertEquals(290.4f, message.getCourseOverGround());
+        assertEquals(2904, message.getRawCourseOverGround());
+        assertEquals(286, message.getTrueHeading());
+        assertEquals(56, message.getSecond());
         assertEquals(ManeuverIndicator.NotAvailable, message.getSpecialManeuverIndicator());
         assertFalse(message.getRaimFlag());
     }
